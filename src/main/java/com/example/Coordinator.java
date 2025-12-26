bdc101e0c47914a4203c4b66eac4d29ee7a2824c
Gleb <glebkabaev200307@gmail.com>
2025-12-26 12:12:38 +0300

    многопоточный MapReduce

--- src/main/java/com/example/Coordinator.java
+++ src/main/java/com/example/Coordinator.java
@@ -1,4 +1,110 @@
 package com.example;
 
+import com.example.task.MapTask;
+import com.example.task.ReduceTask;
+import com.example.task.Task;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Coordinator {
+
+    private final Queue<MapTask> mapTasks = new LinkedList<>();
+    private final Queue<ReduceTask> reduceTasks = new LinkedList<>();
+
+    private int runningMapTasks = 0;
+    private int runningReduceTasks = 0;
+    private boolean isMapPhase = true;
+    private boolean isFinished = false;
+
+    private final int numReduces;
+    private final int totalMapTasks;
+
+
+    private final Lock lock = new ReentrantLock();
+
+    private final Condition tasksChanged = lock.newCondition();
+
+    public Coordinator(List<String> files, int numReduces) {
+        this.numReduces = numReduces;
+        this.totalMapTasks = files.size();
+
+        for (int i = 0; i < files.size(); i++) {
+            mapTasks.add(new MapTask(i, files.get(i), numReduces));
+        }
+    }
+
+    public Task getTask() {
+        lock.lock();
+        try {
+            while (true) {
+                if (isFinished) {
+                    return null;
+                }
+
+                if (isMapPhase) {
+
+                    if (!mapTasks.isEmpty()) {
+                        runningMapTasks++;
+                        return mapTasks.poll();
+                    } else if (runningMapTasks > 0) {
+                        try {
+                            tasksChanged.await();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                            return null;
+                        }
+                    } else {
+                        isMapPhase = false;
+                        initReduceTasks();
+                        tasksChanged.signalAll();
+                    }
+                } else {
+
+                    if (!reduceTasks.isEmpty()) {
+                        runningReduceTasks++;
+                        return reduceTasks.poll();
+                    } else if (runningReduceTasks > 0) {
+                        try {
+                            tasksChanged.await();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                            return null;
+                        }
+                    } else {
+                        isFinished = true;
+                        tasksChanged.signalAll();
+                        return null;
+                    }
+                }
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void completeTask(Task task) {
+        lock.lock();
+        try {
+            if (task instanceof MapTask) {
+                runningMapTasks--;
+            } else if (task instanceof ReduceTask) {
+                runningReduceTasks--;
+            }
+            tasksChanged.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private void initReduceTasks() {
+        System.out.println("Map phase end. Generating reduce tasks ");
+        for (int i = 0; i < numReduces; i++) {
+            reduceTasks.add(new ReduceTask(i, totalMapTasks));
+        }
+    }
 }
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,24 +1,56 @@
 package com.example;
 
-
-import com.example.task.MapTask;
-import com.example.task.ReduceTask;
+import com.example.task.Task;
 
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
 
-        MapTask mapTask= new MapTask(12,"data.txt",1);
+    public static void main(String[] args) throws InterruptedException {
 
-        ReduceTask reduceTask = new ReduceTask(12,1);
-        Worker worker = new Worker();
-        try {
-            worker.work(mapTask);
-            worker.work(reduceTask);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        List<String> inputFiles = Arrays.asList("file1.txt", "file2.txt", "file3.txt");
+
+
+        int workers = 3;
+        int reduces = 3;
+
+        Coordinator coordinator = new Coordinator(inputFiles, reduces);
+
+
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < workers; i++) {
+            Thread thread = new Thread(() -> {
+                Worker worker = new Worker();
+                while (true) {
+                    Task task = coordinator.getTask();
+
+                    if (task == null) {
+                        break;
+                    }
+
+                    try {
+                        worker.work(task);
+                        coordinator.completeTask(task);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            thread.setName("Worker-" + i);
+            thread.start();
+            threads.add(thread);
         }
+
+
+        for (Thread t : threads) {
+            t.join();
+        }
+
+        System.out.println("MapReduce end");
     }
 }
